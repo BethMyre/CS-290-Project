0e73b53d8cc60353777c125432a4b366da512c92
BethMyre <myreel@oregonstate.edu>
2019-05-25 17:21:43 -0400

    Two-letter codes of detected languages are displayed.

--- script.js
+++ script.js
@@ -11,12 +11,28 @@ function bindButtons(){
         req.open("GET", "https://api.dandelion.eu/datatxt/li/v1?text=" + textInput + "&token=" + token,
             true);
 
-        //document.getElementById("textYouEntered").textContent = textInput;
-
         req.addEventListener('load',function(){
             if(req.status >= 200 && req.status < 400){
+                document.getElementById("langsDetected").textContent = "Language(s) detected...";
+
                 var response = JSON.parse(req.responseText);
-                document.getElementById("textYouEntered").textContent = "something happened";
+                console.log(response);
+                var detectedLanguages = response.detectedLangs;
+                console.log(detectedLanguages);
+
+                var langToDisplay;
+                var nameOfLanguage;
+
+                for(let i = 0; i < detectedLanguages.length; i++){
+                    langToDisplay = document.createElement("li");
+                    document.getElementById("langList").appendChild(langToDisplay);
+                    nameOfLanguage = document.createTextNode(detectedLanguages[i].lang);
+                    langToDisplay.appendChild(nameOfLanguage);
+                }
+
+
+
+
                 //document.getElementById('cityID').textContent = response.name;
                 //var fTemp = response.main.temp*9/5 - 459.67;
                 //fTemp = fTemp.toFixed(2);
@@ -25,28 +41,6 @@ function bindButtons(){
             } else {
                 console.log("Error in network request: " + req.statusText);
             }});
-
-       /*
-    document.getElementById("cityButton").addEventListener('click', function(event){
-
-        var req = new XMLHttpRequest();
-        var city = document.getElementById("cityInput").value;
-        req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=" + apiKey, true);
-        req.addEventListener('load',function(){
-            if(req.status >= 200 && req.status < 400){
-                var response = JSON.parse(req.responseText);
-                document.getElementById('cityID').textContent = response.name;
-                var fTemp = response.main.temp*9/5 - 459.67;
-                fTemp = fTemp.toFixed(2);
-                document.getElementById('tempID').textContent = fTemp;
-                document.getElementById('humidityID').textContent = response.main.humidity;
-            } else {
-                console.log("Error in network request: " + req.statusText);
-            }});
-        req.send(null);
-        event.preventDefault();
-    });
-        */
 
         req.send(null);
         event.preventDefault();
