9a9118d195d221d40ac125fcc56129bca6f7b34b
BethMyre <myreel@oregonstate.edu>
2019-05-25 16:14:15 -0400

    Got a response from the API that wasn't an error!

--- script.js
+++ script.js
@@ -4,32 +4,51 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 
 function bindButtons(){
     document.getElementById("langIDButton").addEventListener('click', function(event){
+        var token = "991a606acccf45aeb0b29edd5cb8c521";
         var req = new XMLHttpRequest();
 
-        var testInput;
+        var textInput = document.getElementById("langIDInput").value;
+        req.open("GET", "https://api.dandelion.eu/datatxt/li/v1?text=" + textInput + "&token=" + token,
+            true);
 
-        var payload = {textEntered:null};
-        payload.textEntered = document.getElementById("langIDInput").value;
+        //document.getElementById("textYouEntered").textContent = textInput;
 
-        testInput = payload.textEntered;
-        document.getElementById("textYouEntered").textContent = testInput;
-
-        /*
-        var req = new XMLHttpRequest();
-        var payload = {animal:null};
-        payload.animal = document.getElementById("animalInput").value;
-        req.open("POST", "http://httpbin.org/post", true);
-        req.setRequestHeader('Content-Type', 'application/json');
         req.addEventListener('load',function(){
             if(req.status >= 200 && req.status < 400){
                 var response = JSON.parse(req.responseText);
-                document.getElementById('animalID').textContent = response.json.animal;
+                document.getElementById("textYouEntered").textContent = "something happened";
+                //document.getElementById('cityID').textContent = response.name;
+                //var fTemp = response.main.temp*9/5 - 459.67;
+                //fTemp = fTemp.toFixed(2);
+                //document.getElementById('tempID').textContent = fTemp;
+                //document.getElementById('humidityID').textContent = response.main.humidity;
             } else {
                 console.log("Error in network request: " + req.statusText);
             }});
-        req.send(JSON.stringify(payload));
+
+       /*
+    document.getElementById("cityButton").addEventListener('click', function(event){
+
+        var req = new XMLHttpRequest();
+        var city = document.getElementById("cityInput").value;
+        req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&appid=" + apiKey, true);
+        req.addEventListener('load',function(){
+            if(req.status >= 200 && req.status < 400){
+                var response = JSON.parse(req.responseText);
+                document.getElementById('cityID').textContent = response.name;
+                var fTemp = response.main.temp*9/5 - 459.67;
+                fTemp = fTemp.toFixed(2);
+                document.getElementById('tempID').textContent = fTemp;
+                document.getElementById('humidityID').textContent = response.main.humidity;
+            } else {
+                console.log("Error in network request: " + req.statusText);
+            }});
+        req.send(null);
+        event.preventDefault();
+    });
         */
 
+        req.send(null);
         event.preventDefault();
     });
 }
